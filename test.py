28daecb10741cb91e0c00f3ba62bd1c48061123b
Dominik Bizoň <83905992+bysonD@users.noreply.github.com>
2025-11-14 13:23:38 +0100

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from file_handler import save_repository_to_files, load_repository_from_files
-from quick_start import start
-
-if __name__ == "__main__":
-    start()
-
-    save_repository_to_files()
